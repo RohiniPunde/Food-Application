000c6021e06c3aea096d248ca687223b91676e27
RohiniPunde <punderohini@gmail.com>
2020-09-16 17:43:28 -0700

    added boostrap layout and signals

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import  User
+from django.urls import reverse
 
 class Item(models.Model):
+    user_name=models.ForeignKey(User,on_delete=models.CASCADE,default= 1)
     item_name=models.CharField(max_length=100)
     item_desc=models.CharField(max_length=200)
     item_price=models.IntegerField()
@@ -9,4 +12,8 @@ class Item(models.Model):
 
 
     def __str__(self):
-        return self.item_name 
+        return self.item_name
+
+
+    def get_absolute_url(self):
+        return reverse('food:detail',kwargs={'pk': self.pk})
